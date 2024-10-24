aec198a9a1fec85ffbe93960e9cc6f19bdbf45f8
creepahh <creepahh13@gmail.com>
2024-10-24 09:39:41 +0545

    fixed two routes

--- asynch/script.js
+++ asynch/script.js
@@ -38,22 +38,6 @@
 
 
 
-// const DEMO_URL = "https://jsonplaceholder.typicode.com/posts";
-
-// const request = fetch(DEMO_URL).then((response) => {
-//     console.log(response)
-//     return response.json()
-// })
-//     .then((data) => {
-//         console.log(data);
-//         // console.log(`ID: ${data.id}`);
-//         // console.log(`Title: ${data.title}`);
-//     })
-//     .catch((error) => {
-//         console.log("Error:", error);
-//     });
-
-// console.log(request); 
 
 
 function fetchData(url, callback) {
--- node/server.js
+++ node/server.js
@@ -1,35 +1,50 @@
 import http from "http";
+import fs from 'fs';
+import url from 'url';
 
-//creating http server
-// const server = http.createServer((req, res) => {
-//     console.log(req.url);
-//     console.log(req.method);
-
-//     console.log("incoming request ");
-//     res.end("Hello\n");
-// });
-
-// const PORT = 3000;
-// server.listen(PORT, () => {
-//     console.log(`Server running at http://localhost:${PORT}/`);
-// });
+// const http = require('http');
 
 const server = http.createServer((req, res) => {
-    const url = req.url;
-    switch (url) {
-        case "/":
-            res.end("<h1>Home Page</h1>");
-            break;
-        case "/about":
-            res.end("<h1>About Page</h1>");
-            break;
-        case "/blog":
-            res.end("<h1>Blog Page</h1>");
-            break;
-        case "/contact":
-            res.end("<h1>Contact Page</h1>");
-            break;
-        default:
-            res.end("<h1>Page not found!</h1>");
-    }
+    // console.log(req.url);   
+    // console.log(req.method);
+    
+
+    // console.log("Incoming request ");
+    
+    // res.end("Hello\n");  // Sends a simple response to the client
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+
+    fs.readFile('post-lists.json', 'utf8', (err, data) => {
+        if (err) {
+            res.statusCode = 500;
+            res.end("Error reading file");
+            return;
+        }
+
+        const posts = JSON.parse(data);
+
+        if (path === '/') {
+            // Return all posts
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(posts));
+        }
+        else {
+            const postId = path.slice(1);  //slicing off / from the path to get _id
+            const post = posts.find(p => p._id === postId);         //searching to match 
+        }
+
+        if (!post) {
+            res.statusCode = 404;
+            res.end("Post not found");
+        }
+           
+    });
+
+    // Listening on port 3000
+    server.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+    
 });
